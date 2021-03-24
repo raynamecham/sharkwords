1a38f974d9197b12da3c449d0bec1cb57cd4f8db
Rayna Mecham <rayna.mecham@gmail.com>
2021-03-23 22:06:00 -0600

    finished exercise

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -19,13 +19,17 @@ let numWrong = 0;
 // Loop over the chars in `word` and create divs.
 //
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  for (letter of word ) {
+    $('#word-container').append(`<div class="letter-box ${letter}"></div>`);
+  }
 };
 
 // Loop over each letter in `ALPHABET` and generate buttons.
 //
 const generateLetterButtons = () => {
-  // Replace this with your code
+  for (letter of ALPHABET) {
+    $('#letter-buttons').append(`<button>${letter}</button>`);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -33,19 +37,21 @@ const generateLetterButtons = () => {
 // `buttonEl` is an `HTMLElement` object.
 //
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+  $(buttonEl).attr('disabled', true);
 };
 
 // Return `true` if `letter` is in the word.
 //
 const isLetterInWord = (letter) => {
-  // Replace this with your code
+  let letterInWord =$(`div.${letter}`)[0] !== undefined;
+  return letterInWord;
 };
 
 // Called when `letter` is in word. Update contents of divs with `letter`.
 //
 const handleCorrectGuess = (letter) => {
-  // Replace this with your code
+  let correctGuess =$(`div.${letter}`).html(letter);
+  return correctGuess;  
 };
 
 // Called when `letter` is not in word.
@@ -54,7 +60,15 @@ const handleCorrectGuess = (letter) => {
 // message. Otherwise, increment `numWrong` and update the shark image.
 //
 const handleWrongGuess = () => {
-  // Replace this with your code
+  
+  numWrong +=1;
+
+  $('#shark-img img').attr('src', `/static/images/guess${numWrong}.png`);
+
+  if (numWrong === 5) {
+    $('button').attr('disabled', true);
+    $('#play-again').css({ display: 'block' });
+  }
 };
 
 //  Reset game state. Called before restarting the game.
